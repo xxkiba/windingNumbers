a0d640f5580634e766d66dd33aa37690f2da21c1
flyskywalkerlby <1694584152@qq.com>
2024-05-10 02:52:26 -0400

    cache

--- load_data.py
+++ load_data.py
@@ -1,8 +1,21 @@
+import json
+import os
+
 import open_clip
 import torch
 from torchvision import datasets
 import numpy as np
 from tqdm import tqdm
+
+
+def load_json(path):
+    with open(path, "r") as f:
+        return json.load(f)
+
+
+def dump_json(obj, path, indent=None):
+    with open(path, "w") as f:
+        json.dump(obj, f, indent=indent)
 
 
 def load_data(limit=-1):
@@ -21,18 +34,40 @@ def load_data(limit=-1):
     embeddings = []
     labels = []
 
+    cache_dir = "cache/train"
+    os.makedirs(cache_dir, exist_ok=True)
+
     # Process a small subset of the dataset, for example, the first 1000 images
     for i, (image, label) in enumerate(tqdm(trainset)):
         if limit != -1:
             if i >= limit:  # Limit the number of images processed
                 break
-        preprocessed_image = preprocess(image).unsqueeze(0)
 
-        with torch.no_grad():
-            image_embedding = model.encode_image(preprocessed_image)
-            image_embedding /= image_embedding.norm(dim=-1, keepdim=True)
+        path = f"{cache_dir}/{i}.json"
+        try:
+            el = load_json(path)
+            image_embedding = np.array(el["e"])
+            label = el["l"]
+            # print(f"cache {i}")
 
-        embeddings.append(image_embedding.squeeze().numpy())  # Remove batch dimension and convert to numpy
+        except:
+            preprocessed_image = preprocess(image).unsqueeze(0)
+            with torch.no_grad():
+                image_embedding = model.encode_image(preprocessed_image)
+                image_embedding /= image_embedding.norm(dim=-1, keepdim=True)
+            image_embedding = image_embedding.squeeze().numpy()
+
+            # print(type(image_embedding))
+            # print(type(label))
+
+            dump_json(dict(
+                e=image_embedding.tolist(),
+                l=label,
+            ), path)
+
+            # quit()
+
+        embeddings.append(image_embedding)  # Remove batch dimension and convert to numpy
         labels.append(label)
 
     # Convert to numpy arrays
